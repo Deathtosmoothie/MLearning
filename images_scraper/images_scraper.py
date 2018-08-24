3ec35f194626e22f9cf8339e8113fed5b6ae4965
borschman <russs_sl@mail.ru>
2018-08-24 14:45:33 +0600

    comm added

--- images_scraper/images_scraper.py
+++ images_scraper/images_scraper.py
@@ -7,12 +7,14 @@ import time
 class ImageParser:
     def __init__(self, url):
         self.url = url
-        self.folder = '/home/r/Рабочий_стол/Python/My_Kernels/Object_detection/TOD_video/images/'
-        self.driver = webdriver.Chrome('/home/r/Рабочий_стол/Python/My_Kernels/Object_detection/TOD_video/chromedriver')
+        self.folder = '/path/to/folder/for/downloaded/images/'
+        self.driver = webdriver.Chrome('/path/to/chromedriver')
         self.driver.get(self.url)
 
     def scroller(self):
-
+        # Function for scrolling Google page to load more images
+        
+        # Time for pause while page is loading
         scroll_pause_time = 0.5
 
         # Get scroll height
@@ -33,29 +35,36 @@ class ImageParser:
 
     def downloader(self, fin_link):
 
+        # Path for downloaded image
         dest = self.folder + fin_link.rsplit('/', 1)[1]
 
+        # Headers for request
         opener = build_opener()
         opener.addheaders = [('User-Agent',
                               'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
         install_opener(opener)
 
         try:
+            # Function for download
             urlretrieve(fin_link, dest)
         except Exception as e:
+            # Print errors if occur
             print(str(e))
 
     def chromeImageParser(self):
 
+        # Find all <a> tags which contain <img> tag
         a_tag = self.driver.find_elements_by_xpath('//a[img]')
 
         for a in a_tag:
 
+            # Get href attribute of <a>
             href = a.get_attribute("href")
 
             if href is None:
                 continue
-            
+
+            # Parse href value
             prs = urlparse(href)
             print(prs)
             prs_q = unquote(prs.query)
@@ -64,6 +73,7 @@ class ImageParser:
             prs_str_end = "jpg"
             prs_str_alt = "png"
 
+            # Building final link for download full size image
             if prs_str_start and prs_str_end in prs_q:
                 fin_link = prs_q.split(prs_str_start)[1].split(prs_str_end)[0] + "jpg"
                 print(fin_link)
@@ -81,6 +91,6 @@ class ImageParser:
 
 
 if __name__=='__main__':
-    IP = ImageParser("https://www.google.ru/search?q=Dave+Grohl&newwindow=1&tbm=isch&source=lnt&tbs=isz:m&sa=X&ved=0ahUKEwjw1dLF_PPcAhXkNJoKHRYdBDkQpwUIHg&biw=1366&bih=602&dpr=1")
+    IP = ImageParser("paste-here-link-from-google")
     IP.scroller()
     IP.chromeImageParser()
